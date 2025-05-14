cfd4e435422f3085b45d76df8ba8d8da88a22955
jesusalbujas <jesusramirez35000@gmail.com>
2025-05-13 21:32:16 -0400

    Add validator if exists in database for update or insert

--- src/main/java/org/spin/ams/model/AbstractImporter.java
+++ src/main/java/org/spin/ams/model/AbstractImporter.java
@@ -4,6 +4,7 @@ import org.compiere.model.MTable;
 import org.compiere.model.PO;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
 import java.util.UUID;
 import java.util.Properties;
 
@@ -11,17 +12,23 @@ public abstract class AbstractImporter {
 
     protected abstract String getTableName();
     protected abstract void mapFields(JSONObject json, PO po) throws Exception;
+    protected abstract PO lookupExistingRecord(JSONObject json);
 
     public void importData(JSONArray data, Properties ctx, String trxName) throws Exception {
         for (int i = 0; i < data.length(); i++) {
             JSONObject item = data.getJSONObject(i);
-            PO po = MTable.get(ctx, getTableName()).getPO(0, trxName);
+            PO po = lookupExistingRecord(item);
+
+            if (po == null) {
+                po = MTable.get(ctx, getTableName()).getPO(0, trxName);
+            }
+
             mapFields(item, po);
             po.saveEx();
         }
     }
-    
+
     protected String getUuid() {
-    	return UUID.randomUUID().toString();
+        return UUID.randomUUID().toString();
     }
 }
